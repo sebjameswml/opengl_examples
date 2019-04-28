b6e3210199f6fd149815bce026304cd7de36225e
Seb James <seb.james@sheffield.ac.uk>
2019-04-28 22:36:45 +0100

    Adds a multiple sphere example prog

---
+++ qt/opengl45/sphere2/shapewindow.h
@@ -0,0 +1,48 @@
+#ifndef _SHAPEWINDOW_H_
+#define _SHAPEWINDOW_H_
+
+#include "openglwindow.h"
+#include "spheregeometry.h"
+#include <QtGui/QOpenGLShaderProgram>
+#include <QtGui/QOpenGLContext>
+#include <QtGui/QOpenGLBuffer>
+#include <QtGui/QOpenGLVertexArrayObject>
+
+#include <QMatrix4x4>
+#include <QQuaternion>
+#include <QVector2D>
+#include <QMouseEvent>
+#include <QBasicTimer>
+
+#include <vector>
+
+// header style declaration
+class ShapeWindow : public OpenGLWindow
+{
+public:
+    ShapeWindow();
+    ~ShapeWindow();
+
+    void mousePressEvent(QMouseEvent *e) override;
+    void mouseReleaseEvent(QMouseEvent *e) override;
+    void timerEvent(QTimerEvent*) override;
+
+    void initialize() override;
+    void render() override;
+
+    void setPerspective (int w, int h);
+
+private:
+    QVector2D mousePressPosition;
+    QVector3D rotationAxis;
+    qreal angularSpeed;
+    QQuaternion rotation;
+    QMatrix4x4 projection;
+
+    QBasicTimer timer;
+
+    QOpenGLShaderProgram* shaderProg;
+    SphereGeometry* sphere;
+};
+
+#endif // _SHAPEWINDOW_H_
